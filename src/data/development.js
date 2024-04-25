09b8529d53e5a5b529f174789acfcf6bf737931f
Fabrício Macena <fabriciomacena03@gmail.com>
2024-04-25 18:03:29 -0300

    projeto crud-spring-angular inserido

--- src/data/development.js
+++ src/data/development.js
@@ -1,6 +1,15 @@
 export const dataDEV = [
     {
         "id": 0,
+        "title": "Gerenciador de Produtos",
+        "description": "Uma aplicação desenvolvida com Angular e Spring Boot, que funciona como um eficiente gerenciador de produtos. Esta plataforma oferece uma ampla gama de funcionalidades, incluindo as operações básicas de CRUD (Create, Read, Update, Delete). Com ela, os usuários podem facilmente visualizar, cadastrar, atualizar e excluir produtos conforme necessário, proporcionando uma experiência fluida e intuitiva de gerenciamento de inventário.",
+        "src": require("../assets/videos/prodmanager-mp4.mp4"),
+        "typeSrc": "mp4",
+        "link": "https://github.com/FabricioMacena/crud-spring-angular",
+        "badges": ["Spring", "Angular", "Javascript", "MySQL", "Javascript", "Sass"]
+    },
+    {
+        "id": 1,
         "title": "Identificador De Imagens",
         "description": "Através do uso de Redes Neurais Convolucionais e técnicas de Machine Learning, desenvolvi uma aplicação com o objetivo de identificar qual animal está presente em uma foto enviada pelo usuário.",
         "src": require("../assets/videos/recognizer-mp4.mp4"),
@@ -9,7 +18,7 @@ export const dataDEV = [
         "badges": ["Python", "Flask", "Javascript", "Tensorflow"]
     },
     {
-        "id": 1,
+        "id": 2,
         "title": "Find Jobs",
         "description": "Desenvolvi uma aplicação utilizando Flask e React que automatiza a busca de vagas de emprego nos principais sites do Brasil, utilizando Selenium e Beautiful Soup 4.",
         "src": require("../assets/videos/findjobs-mp4.mp4"),
@@ -18,7 +27,7 @@ export const dataDEV = [
         "badges": ["Python", "Flask", "Javascript", "React", "React-Router", "Styled-Components", "Selenium", "Beautifulsoup"]
     },
     {
-        "id": 2,
+        "id": 3,
         "title": "Análise de Sentimentos",
         "description": "Essa aplicação utiliza um algoritmo para traduzir e analisar frases, prevendo se são positivas ou negativas.",
         "src": require("../assets/videos/sentimentanalysis-mp4.mp4"),
